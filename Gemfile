1e23a8efdcaf3afcfe029fed8b6e12c711855829
Łukasz Korecki <lukasz@coffeesounds.com>
2014-12-09 11:38:22 +0000

    testing git based dependency

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source 'https://rubygems.org'
 gem 'sinatra'
 gem 'foreman'
 gem 'unicorn'
+
+gem 'sinatra-contrib', git: 'git@github.com:sinatra/sinatra-contrib.git'
--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
-
 require 'sinatra'
+require 'sinatra/json'
 
-get '/foo/:key' do |key|
-  ENV[key]
+get '/' do
+  json ENV.to_h
+end
+
+get '/env/:key' do |key|
+  json key => ENV[key.upcase]
 end
